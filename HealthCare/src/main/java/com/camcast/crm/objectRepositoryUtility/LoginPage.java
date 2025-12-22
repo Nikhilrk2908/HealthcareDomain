cfd97a85b9284d432cbbad96d24ccb70f6458876
Praveen <praveensingam4@gmail.com>
2025-12-22 15:28:44 +0530

    first commit from praveensingam

--- HealthCare/src/main/java/com/camcast/crm/objectRepositoryUtility/LoginPage.java
+++ HealthCare/src/main/java/com/camcast/crm/objectRepositoryUtility/LoginPage.java
@@ -8,42 +8,41 @@ import org.openqa.selenium.support.PageFactory;
 import com.comcast.crm.generic.webDriverUtility.WebDriverUtility;
 
 public class LoginPage {
-	WebDriverUtility wlib=new WebDriverUtility();
+	WebDriverUtility wlib = new WebDriverUtility();
 	WebDriver driver;
+
 	public LoginPage(WebDriver driver) {
-		this.driver=driver;
+		this.driver = driver;
 		PageFactory.initElements(driver, this);
 	}
 
 	@FindBy(name = "user_name")
-	private WebElement usernameEdt;
+	private WebElement usernameTf;
 
 	@FindBy(name = "user_password")
-	private WebElement userpasswordEdt;
+	private WebElement userpassworTf;
 
 	@FindBy(id = "submitButton")
 	private WebElement loginBtn;
 
-	public WebElement getUsernameEdt() {
-		return usernameEdt;
+	public void enterUsername(String username) {
+		usernameTf.sendKeys(username);
 	}
 
 	public WebElement getUserpasswordEdt() {
-		return userpasswordEdt;
+		return userpassworTf;
 	}
 
 	public WebElement getLoginBtn() {
 		return loginBtn;
 	}
 
-	public void loginToApp(String url,String username, String password) {
+	public void loginToApp(String url, String username, String password) {
 		driver.get(url);
-		usernameEdt.sendKeys(username);
-		userpasswordEdt.sendKeys(password);
+		usernameTf.sendKeys(username);
+		userpassworTf.sendKeys(password);
 		loginBtn.click();
-		wlib.ToMaximizeWindow(driver);
-		wlib.waitForPageToLoad(driver);;
-		
+		wlib.waitForPageToLoad(driver);
 	}
 
 }
